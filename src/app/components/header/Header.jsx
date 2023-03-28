592c969c0e9fbd54e3ac7162f6d99c7a4fd639d9
Yuriy Pevzner <yuriy.pevzner@todor3d.com>
2023-03-29 01:05:02 +0200

    header adaptation and full card animation

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Icons from '../../../assets/icons/Icons'
+import useWindowSize from '../../hook/resizeWindow'
 import asideBar from '../asideBar'
 import AsideBar from '../asideBar/App'
 import './style.scss'
 
 const Header = ({ asideBar }) =>{
 
+    const windowSize = useWindowSize()
     const header = useRef()
     const [style, setStyle] = useState(false)
     const count = useSelector(state => state.store.count)
@@ -22,16 +24,31 @@ const Header = ({ asideBar }) =>{
             ref={header}    
         >
             <div className="header-content">
+                {
+                    windowSize.width < 1180 &&
+                    <div className="header-content-mobNav">
+                        <div className="header-content-mobNav__line"></div>
+                        <div className="header-content-mobNav__line"></div>
+                        {/* <div className="header-content-mobNav__line"></div> */}
+                        <div className="header-content-mobNav__line"></div>
+                    </div>
+                }
                 <div className="header-content-logo center">
                     ASH
                 </div>
-                <nav className='header-content-nav'>
-                    <ul>
-                        <li>ДОСТАВКА</li>
-                        <li>О НАС</li>
-                        <li>О НАС</li>
-                    </ul>
-                </nav>
+                {
+                    windowSize.width > 1180 &&
+                    <nav className='header-content-nav'>
+                        <ul>
+                            <li>ДОСТАВКА</li>
+                            <li>О НАС</li>
+                            <li>О НАС</li>
+                        </ul>
+                    </nav>
+                }
+                <div className='header-content-phone center'>
+                    +380 (050) 687-59-30
+                </div>
                 <div className="header-content-basket center"
                     onClick={asideBar}
                 >
